94f55fcd2c7b6190ba6e07bc1e26219a569db3f5
BioticComponent <juzdzinski@gmail.com>
2020-02-12 18:41:29 -0500

    v1 of recursiveRoadGen implemented

--- src/cell.js
+++ src/cell.js
@@ -1,11 +1,19 @@
 class Cell {
-    constructor(i,j,pos) {
-        this.i = i;
-        this.j = j;
-        this.pos = pos;
+    constructor(i,j,pos, designation) {
+        this.i = i; //i-index of cell
+        this.j = j; //j-index of cell
+        this.pos = pos; //position coordinates (x,y)
+        this.designation = designation; //0 for land cell, 1 for road cell
+        this.neighbors = []; //array of surrounding road cells for each road cell, empty if land cell
     }
 
     show() {
+        noStroke();
+        if (this.designation == 1) { //is roadCell 
+            fill(0);
+        } else { //is landCell
+            fill(255);
+        }
         rect(this.pos.x, this.pos.y, width/dim, width/dim)
     }
 
--- src/sketch.js
+++ src/sketch.js
@@ -1,13 +1,16 @@
 let allCells = [];
 let landCells = [];
 let roadCells = [];
-let dim = 40; //size of grid
+let dim = 100; //size of grid
+
+let counter = 0;
 
 function setup() {
-    let canvasSize = 600
+    let canvasSize = 600;
     createCanvas(canvasSize,canvasSize);
     background(50);
     initializeCellGrid();
+    recursiveRoadGeneration(0, floor(dim / 2), 'r');
 }
 
 function draw() {
@@ -15,6 +18,87 @@ function draw() {
     for (let cell of allCells) {
         cell.show();
     }
+    noLoop();
+}
+//create road map
+function recursiveRoadGeneration(a, b, dir) {
+    let roadLength;
+    let stopValue = 10;
+    if (dir == 'r') {
+        roadLength = makeRoad(a, b, dir);
+        if (roadLength > stopValue) {
+            recursiveRoadGeneration(a + floor((roadLength/2) + random(-(roadLength/4),(roadLength/4))), b, 'u');
+            recursiveRoadGeneration(a + floor((roadLength/2) + random(-(roadLength/4),(roadLength/4))), b, 'd');
+        }
+    } else if (dir == 'd') {
+        roadLength = makeRoad(a, b, dir);
+        if (roadLength > stopValue) {
+            recursiveRoadGeneration(a, b + floor((roadLength/2) + random(-(roadLength/4),(roadLength/4))), 'l');
+            recursiveRoadGeneration(a, b + floor((roadLength/2) + random(-(roadLength/4),(roadLength/4))), 'r');
+        }
+    } else if (dir == 'l') {
+        roadLength = makeRoad(a, b, dir);
+        if (roadLength > stopValue) {
+            recursiveRoadGeneration(a - floor((roadLength/2) + random(-(roadLength/4),(roadLength/4))), b, 'u');
+            recursiveRoadGeneration(a - floor((roadLength/2) + random(-(roadLength/4),(roadLength/4))), b, 'd');
+        }
+    } else if (dir == 'u') {
+        roadLength = makeRoad(a, b, dir);
+        if (roadLength > stopValue) {
+            recursiveRoadGeneration(a, b - floor((roadLength/2) + random(-(roadLength/4),(roadLength/4))), 'l');
+            recursiveRoadGeneration(a, b - floor((roadLength/2) + random(-(roadLength/4),(roadLength/4))), 'r');
+        }
+    }
+    
+}
+
+//returns index in allCells that cell with i,j resides in
+function cellIndex(i, j) {
+    return i * dim + j;
+}
+
+//make a road from (a,b) to x spaces in direction dir
+function makeRoad(a,b, dir) {
+    if (dir != 'r' && dir != 'd' && dir != 'l' && dir != 'u') {
+        noLoop();
+        console.log("direction not recognized in call to makeRoad()");
+        return;
+    }
+    let i = 0; 
+    let counter2 = 0;
+    let roadLength = 0;
+    while (1) {
+        let cell;
+        if (dir == 'r') {
+            if (!(cell = allCells[cellIndex(a + i, b)]) || a+i == dim) {
+                return roadLength;
+            }
+        } else if (dir == 'd') {
+            if (!(cell = allCells[cellIndex(a, b + i)]) || b+i == dim) {
+                return roadLength;
+            }
+        } else if (dir == 'l') {
+            if (!(cell = allCells[cellIndex(a - i, b)]) || a-i < 0) {
+                return roadLength;
+            }
+        } else if (dir == 'u') {
+            if (!(cell = allCells[cellIndex(a, b - i)]) || b-i < 0) {
+                return roadLength;
+            }
+        }
+        if (cell.designation == 1 && counter2 > 0) {    
+            return roadLength;
+        }
+        cell.designation = 1;
+        roadCells.push(cell);
+        landCells.splice(getIndexInLC(cell), 1);
+        i++; counter2++; roadLength++;
+    }
+}
+
+//get cell index in landCells array
+function getIndexInLC(cell) {
+    return landCells.indexOf(cell);
 }
 
 //create grid of cells and insert them into cells array
@@ -22,7 +106,7 @@ function initializeCellGrid() {
     for (let i = 0; i < dim; i++) {
         for (let j = 0; j < dim; j++) {
             let pos = createVector(i * (width/dim), j * (width/dim));
-            let cell = new Cell(i,j,pos);
+            let cell = new Cell(i,j,pos, 0);
             allCells.push(cell);
             landCells.push(cell);
         }
