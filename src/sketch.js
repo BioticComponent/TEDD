bbf051e59052141959be86729f8007cfd50d252a
BioticComponent <juzdzinski@gmail.com>
2020-02-11 21:20:57 -0500

    edit cell arrays

--- src/sketch.js
+++ src/sketch.js
@@ -1,25 +1,30 @@
-let cells = [];
-let dim = 10; //size of grid
+let allCells = [];
+let landCells = [];
+let roadCells = [];
+let dim = 40; //size of grid
 
 function setup() {
-    createCanvas(400,400);
+    let canvasSize = 600
+    createCanvas(canvasSize,canvasSize);
     background(50);
     initializeCellGrid();
 }
 
 function draw() {
     background(50);
-    for (let cell of cells) {
+    for (let cell of allCells) {
         cell.show();
     }
 }
 
+//create grid of cells and insert them into cells array
 function initializeCellGrid() {
     for (let i = 0; i < dim; i++) {
         for (let j = 0; j < dim; j++) {
             let pos = createVector(i * (width/dim), j * (width/dim));
             let cell = new Cell(i,j,pos);
-            cells.push(cell);
+            allCells.push(cell);
+            landCells.push(cell);
         }
     }
 }
